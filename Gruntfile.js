c40ca3d71a5a70d8f8b3eae1cb7c26d4408d226e
Steve Barbera <me@stevebarbera.com>
2014-09-20 14:54:47 -0600

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt){
+
+	grunt.initConfig({
+		jshint: {
+			all: ['Gruntfile.js', 'hello.js']
+		}
+	}); 
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.registerTask('default', ['jshint']);
+
+};
+
